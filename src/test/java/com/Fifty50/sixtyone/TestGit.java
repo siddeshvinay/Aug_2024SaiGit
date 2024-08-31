2680ea21a914997679214d7534e900f2fd6e0333
siddeshvinay <siddeshvinay@gmail.com>
2024-08-31 13:02:03 +0530

    From second branch

---
+++ src/test/java/com/Fifty50/sixtyone/TestGit.java
@@ -0,0 +1,13 @@
+package com.Fifty50.sixtyone;
+
+public class TestGit {
+
+    public static void newtestadd(){
+        System.out.println(5+5);
+    }
+
+    public static void main(String[] args) {
+        newtestadd();
+        System.out.println("new begining");
+    }
+}
